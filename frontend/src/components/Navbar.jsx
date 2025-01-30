7c90bdfd89c07f0f122bb7dedb389efaaea410f5
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-01-30 18:51:35 -0300

    ADD: Hover ao navibar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,16 +5,16 @@ const Navbar = () => {
     <header className="fixed-nav-bar w-nav">
       <nav className="max-w-screen-2x1 mx-auto px-4 flex items-center justify-between">
         <ul className="nav__links">
-          <li>
+          <li className="link">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/shop">Shop</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/pages">Pages</Link>
           </li>
-          <li>
+          <li className="link">
             <Link to="/contact">contact</Link>
           </li>
         </ul>
@@ -24,9 +24,27 @@ const Navbar = () => {
             Lebaba<span>.</span>
           </Link>
         </div>
+
         {/*nav icon*/}
-        <div className="nav__icon relative">
-          <Link></Link>
+        <div className="nav__icons relative">
+          <span>
+            <Link to="/search">
+              <i className="ri-search-line"></i>
+            </Link>
+          </span>
+          <span>
+            <button className="hover:text-primary">
+              <i className="ri-shopping-bag-line"></i>
+              <sup className="text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center w-5">
+                0
+              </sup>
+            </button>
+          </span>
+          <span>
+            <Link to="/login">
+              <i className="ri-user-line"></i>
+            </Link>
+          </span>
         </div>
       </nav>
     </header>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { RouterProvider } from 'react-router';
 import router from './routers/router';
+import 'remixicon/fonts/remixicon.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
