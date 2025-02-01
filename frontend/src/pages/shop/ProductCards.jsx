92eb466b077b587e0c8d1e691bfaece3d226f31d
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-02-01 09:00:13 -0300

    ADD: botão para mostrar mais produtos na pagina e estrelas de rankin do produto

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -1,29 +1,33 @@
 import { Link } from "react-router";
-import products from "../../data/products";
+import RatingStars from "../../components/RatingStars";
 
-const ProductCards = () => {
-  console.log(products);
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-      {products.map((product, index) => (
-        <div key={index} className="product__card">
-          <div className="relative">
-            <Link to={`/shop/${product.id}`}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="max-h96 md:h-64 w-full objexct-cover hover:scale-105 transition-all duration-300"
-              />
-            </Link>
-            <div className="hover:block absolute top-3 right-3">
-                      <button><i className="ri-shopping-cart-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i></button>
-            </div>
-          </div>
-          <h4>{product.name}</h4>
+const ProductCards = ({ products }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+            {products.map((product, index) => (
+                <div key={index} className="product__card">
+                    <div className="relative">
+                        <Link to={`/shop/${product.id}`}>
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                className="max-h96 md:h-64 w-full objexct-cover hover:scale-105 transition-all duration-300"
+                            />
+                        </Link>
+                        <div className="hover:block absolute top-3 right-3">
+                            <button><i className="ri-shopping-cart-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i></button>
+                        </div>
+                    </div>
+                    {/* product description */}
+                    <div className="product__card__content">
+                        <h4>{product.name}</h4>
+                        <p>R${product.price} {product?.oldprice ? <s>{product?.oldprice}</s> : null}</p>
+                        <RatingStars rating={product.rating}/>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default ProductCards;
--- frontend/src/pages/shop/Trendingproducts.jsx
+++ frontend/src/pages/shop/Trendingproducts.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import products from "../../data/products";
 import ProductCards from "./ProductCards";
 
 const Trendingproducts = () => {
-    const [visibleProducts, setVisibleProducts] = useState(8)
+    const [visibleProducts, setVisibleProducts] = useState(4)
     const loadMoreProducts = () => {
         setVisibleProducts(preveCount => preveCount + 4)
     }
@@ -10,13 +11,24 @@ const Trendingproducts = () => {
   return (
     <section className="section__container product__container">
       <h2 className="section__header">Tendência de produtos</h2>
-      <p className="section__subheader mb-12">
-        Descubra as escolhas mais quentes: Eleve seu estilo com a nossa coleção
-        com curadoria de produtos de moda feminina de tendência
+      <p className="section__subheader " style={{ marginBottom: "3rem" }}>
+        Descubra as escolhas mais quentes: Eleve seu estilo com a nossa coleção com curadoria de produtos de moda feminina de tendência
       </p>
 
       {/* <!-- Product cards --> */}
-      <ProductCards/>
+      <div>
+        <ProductCards products={products.slice(0, visibleProducts)} />
+      </div>
+      {/*load more products btn */}
+      <div className="product__btn">
+        {
+          visibleProducts < products.length && (
+            <button className="btn" onClick={loadMoreProducts}>Mostrar mais</button>
+          )
+        }
+
+      </div>
+
     </section>
   );
 };
