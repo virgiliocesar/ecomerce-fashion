f08cb90d8974df21ec11daaa4d8ca04f9418e002
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-02-27 17:55:56 -0300

    FEAT(router): adicionado rota edit or update profile

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -107,6 +107,45 @@ router.put("/users/:id", async (req, res) => {
   }
 });
 
+//^ edit or update profile
+router.patch("/edit-profile", async (req, res) => {
+  try {
+    const { userId, username, profileImage, bio, profession } = req.body;
+    if (!userId) {
+      return res.status(400).send({ message: "User ID is required" });
+    }
+    const user = await User.findById(userId);
+    if(!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    //^ update profile
+    if(username !== undefined) user.username = username;
+    if(profileImage !== undefined) user.profileImage = profileImage;
+    if(bio !== undefined) user.bio = bio;
+    if(profession !== undefined) user.profession = profession;
+
+    await user.save();
+    res.status(200).send({
+      message: "User profile updated successfully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        role: user.role,
+        profileImage: user.profileImage,
+        bio: user.bio,
+        profession: user.profession,
+      },
+    });
+    
+  } catch (error) {
+    console.error("Error updating user profile:", error);
+    res.status(400).send({ message: "Error updating user profile" });
+    
+  }
+})
+
+
 // //^ protected route endpoint
 // router.get("/users", verifyToken, async (req, res) => {
 //   res.send({ message: "Protected users" });
