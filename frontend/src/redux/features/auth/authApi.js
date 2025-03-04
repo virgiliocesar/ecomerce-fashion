37e4a8163fecac98307004756ab3f16044f4b64b
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-03-04 14:59:52 -0300

    FEAT(authApi): adicionado useGetUserQuery, useDeleteUserMutation, useUpdateUserRoleMutation, useEditProfileMutation

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -22,9 +22,54 @@ const authApi = createApi({
         body: credentials,
       }),
     }),
+    logoutUser: builder.mutation({
+      query: () => ({
+        url: "/logout",
+        method: "POST",
+      }),
+    }),
+    getUser: builder.query({
+      query: () => ({
+        url: "/users",
+        method: "GET",
+      }),
+      refetchOnMount: true,
+      invalidatesTags: ["User"],
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["User"],
+    }),
+    updateUserRole: builder.mutation({
+      query: (userId, role) => ({
+        url: `/users/${userId}`,
+        method: "PUT",
+        body: { role },
+      }),
+      refetchOnMount: true,
+      invalidatesTags: ["User"],
+    }),
+    editProfile: builder.mutation({
+      query: (profileData) => ({
+        url: `/edit-profile`,
+        method: "PATCH",
+        body: profileData,
+      }),
+    }),
   }),
 });
 
-export const { useRegisterUserMutation, useLoginUserMutation } = authApi;
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation,
+  useGetUserQuery,
+  useDeleteUserMutation,
+  useUpdateUserRoleMutation,
+  useEditProfileMutation,
+} = authApi;
 
 export default authApi;
