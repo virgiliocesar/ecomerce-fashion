5ba44c6d306ed5051fd0438dd9c22689f5d2ed28
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-03-11 16:54:39 -0300

    FEAT(product):criar produto uso de rtk query e api

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,13 +3,15 @@ import cartReducer from "./features/cart/cartSlice"
 
 import authApi from "./features/auth/authApi"
 import authReducer from "./features/auth/authSlice"
+import productsApi from "./products/productsApp";
 
 export const store = configureStore({
     reducer: {
     cart: cartReducer,
     [authApi.reducerPath]: authApi.reducer,
-    auth: authReducer
+    auth: authReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware,productsApi.middleware),
 });
