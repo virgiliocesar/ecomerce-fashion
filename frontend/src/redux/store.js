e862ec8a0e35c2ddbe77c2c4ebd59f170ff28b95
Virgílio César <virgilio_cesar.dev@outlook.com>
2025-04-07 09:50:30 -0300

    FEAT(api):adicionado  Stats Api usado RTK Query

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,6 +5,7 @@ import authApi from "./features/auth/authApi";
 import authReducer from "./features/auth/authSlice";
 import productsApi from "./features/products/productsApi";
 import reviewApi from "./features/reviews/reviewsApi";
+import statsApi from "./features/stats/statsApi";
 
 export const store = configureStore({
   reducer: {
@@ -12,8 +13,9 @@ export const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     auth: authReducer,
     [productsApi.reducerPath]: productsApi.reducer,
-    [reviewApi.reducerPath]: reviewApi.reducer
+    [reviewApi.reducerPath]: reviewApi.reducer,
+    [statsApi.reducerPath]: statsApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware,reviewApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, productsApi.middleware,reviewApi.middleware,statsApi.middleware),
 });
